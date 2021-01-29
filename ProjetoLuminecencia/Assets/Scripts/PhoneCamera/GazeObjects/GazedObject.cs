fadb5ded54924097e9f7babeb8615044195e1bcd
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2021-01-29 12:25:08 -0300

    Changes on GazeObjects script
    made it look for multiple points, intead of just one

--- ProjetoLuminecencia/Assets/Scripts/PhoneCamera/GazeObjects/GazedObject.cs
+++ ProjetoLuminecencia/Assets/Scripts/PhoneCamera/GazeObjects/GazedObject.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class GazedObject : MonoBehaviour
 {
 
-    [SerializeField] private Transform targetPoint;
-    [SerializeField] private Camera gazingCamera;
-
+    [SerializeField] private float percentageOfScreenApearenceAcception = 0.6f;
     [SerializeField] private float seeingDistance = 30f;
+
+    private int pointsSeenNow = 0;
+
+    [SerializeField] private Transform[] targetPointsArray;
+    [SerializeField] private Camera gazingCamera;
 
     private void Update()
     {
 
-        if (IsOnCamera())
+        Debug.Log((float) pointsSeenNow / targetPointsArray.Length);
+
+        if (IsObjectOnCamera())
         {
             Debug.Log("Yes");
         }
@@ -27,9 +32,38 @@ public class GazedObject : MonoBehaviour
 
     #region private functions
 
-    private bool IsOnCamera()
+    private bool IsObjectOnCamera()
     {
-        Vector3 screenPoint = gazingCamera.WorldToViewportPoint(targetPoint.position);
+
+        if (AreAllPointsInFOV())
+        {
+            if (PointsSeenAreEnough())
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+        
+    }
+
+    private bool AreAllPointsInFOV()
+    {
+
+        bool allInFOV = true;
+        foreach(var point in targetPointsArray)
+        {
+            if (IsPointOnFOV(point) == false)
+                allInFOV = false;
+        }
+
+        return allInFOV;
+    }
+
+    private bool IsPointOnFOV(Transform point)
+    {
+        Vector3 screenPoint = gazingCamera.WorldToViewportPoint(point.position);
 
         // Behind camera
         if (screenPoint.z < 0)
@@ -37,28 +71,68 @@ public class GazedObject : MonoBehaviour
 
         // Is on field of view
         if (screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1)
+            return true;
+
+        return false;
+    }
+
+    private bool IsCameraSeeingPoint(Transform point)
+    {
+        Vector3 dir = (point.position - gazingCamera.transform.position).normalized;
+        Ray ray = new Ray(gazingCamera.transform.position, dir);
+
+        
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, seeingDistance))
         {
 
-            Vector3 dir = (targetPoint.position - gazingCamera.transform.position).normalized;
-            Ray ray = new Ray(gazingCamera.transform.position, dir);
-            Debug.DrawRay(gazingCamera.transform.position, dir * seeingDistance, Color.red);
-            RaycastHit hitInfo;
-            if(Physics.Raycast(ray, out hitInfo, seeingDistance))
+
+            var gazedObj = hitInfo.transform.GetComponentInParent<GazedObject>();
+
+            if (GameObject.ReferenceEquals(gazedObj, this))
             {
-
-                var gazedObj = hitInfo.transform.GetComponentInParent<GazedObject>();
-
-                if(GameObject.ReferenceEquals(gazedObj, this))
-                {
-                    return true;
-                }
+                Debug.DrawRay(gazingCamera.transform.position, dir * seeingDistance, Color.green);
+                return true;
             }
-
+            else
+            {
+                Debug.DrawRay(gazingCamera.transform.position, dir * seeingDistance, Color.red);
+            }
 
         }
 
+        Debug.DrawRay(gazingCamera.transform.position, dir * seeingDistance, Color.blue);
+
+
         return false;
-        
+    }
+
+    private int HowManyPointsAreSeen()
+    {
+
+        int numOfPointsSeen = 0;
+        foreach (var point in targetPointsArray)
+        {
+            if (IsCameraSeeingPoint(point))
+            {
+                numOfPointsSeen++;
+            }
+        }
+
+        return numOfPointsSeen;
+
+    }
+
+    private bool PointsSeenAreEnough()
+    {
+        pointsSeenNow = HowManyPointsAreSeen();
+        if (((float) pointsSeenNow / targetPointsArray.Length) > percentageOfScreenApearenceAcception)
+        {
+            return true;
+        }
+
+        return false;
+
     }
     #endregion
 
