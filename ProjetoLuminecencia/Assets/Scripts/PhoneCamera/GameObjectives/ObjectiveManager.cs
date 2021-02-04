689e0ac332c03925e3f23be661cf4b6e35adbeef
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2021-02-04 10:42:19 -0300

    Created objective manager
    Started to make the bases for the gameplay ponctuation

---
+++ ProjetoLuminecencia/Assets/Scripts/PhoneCamera/GameObjectives/ObjectiveManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+
+    private List<PhotoObjective> photoObjectives = null;
+
+    private void Awake()
+    {
+        var gazedObjectsInScene = FindObjectsOfType<GazedObject>().ToList();
+        gazedObjectsInScene.ForEach((gazedObject) => photoObjectives.Add(new PhotoObjective(gazedObject)));
+    }
+
+}
